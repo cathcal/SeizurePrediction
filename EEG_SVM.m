c99d1320e0b015307096b5eb3df7303a0ee79f60
kelkind <katherine.elkind@gmail.com>
2016-12-05 19:17:16 -0500

    EEG_SVM, eeg_read_svm, feature_matrix
    Added capability so that the programs can be run for both training and test applications by entering a '1' for training, '2' for test.

--- EEG_SVM.m
+++ EEG_SVM.m
@@ -6,71 +6,80 @@
 %   trials for each patient. So when the program is called, you can input
 %   which patient you want. 
 
+%% Run Program:
 % when the program is run, it will ask which patient you want to analyze:
 ptNum = input('Patient Number: ');
 % for programming purposes, this asks how many trials to train with
 nTrials = input('Number of Trials: ');
+runType = input('Enter 1 for Train or 2 for Test: ');
+if (runType == 1)
+    runType = 'train';
+elseif (runType == 2)
+    runType = 'test';
+end
 
 nwin = 10;
 % then it will process all of the trials for that patient, by analyzing
 % them one by one in 1-min non-overlapping windows, outputting the feature
 % matrix, and training the SVM accordingly:
 
-[meansTime, meansFFT, stdTime, stdFFT] = deal([]);
+% [meansTime, meansFFT, stdTime, stdFFT, Delt, Thet, Alph, Beta, Gamm] = ...
+%     deal([]);
+% [meansTimeTest, meansFFTTest, stdTimeTest, stdFFTTest, DeltTest, ...
+% ThetTest, AlphTest, BetaTest, GammTest] = deal([]);
 
 % for loop will concatenate the data continuously onto the file through the
 % various trials
-for j = 1:nTrials
-    featMat = feature_matrix(ptNum, j);
-    for k = 1:nwin
-        fn = strcat('Min_',num2str(k));
-        meansTime = [meansTime ; featMat.(fn)(:,1:2)];
-        meansFFT = [meansFFT ; featMat.(fn)(:,3:4)];
-        stdTime = [stdTime ; featMat.(fn)(:,5:6)];
-        stdFFT = [stdFFT ; featMat.(fn)(:,7:8)];
+if strcmp(runType,'train')
+    nTrain = floor(.7*nTrials);
+    inputVect = [];
+    Y = repmat([zeros([16,1]) ; ones([16,1])],[10,1]);
+    for j = 1:nTrain
+        featMat = feature_matrix(ptNum, j, runType);
+        for k = 1:nwin
+            fn = strcat('Min_',num2str(k));
+            minint = featMat.(fn)(:,1:19);
+            minpre = featMat.(fn)(:,20:38);
+            inputVect = abs([inputVect; minint; minpre]);
+        end
     end
+    Y = repmat(Y,[j,1]);
+    
+    EEGSVMModel = fitcsvm(inputVect,Y);
+    
+    nTest = ceil(.3*nTrials);
+    testVect = [];
+    % Ytest = repmat([0 ; 1],[10,1]);
+    for j = 1:nTest
+        testMat = feature_matrix(ptNum, 25, runType);
+        for k = 1:nwin
+            fn = strcat('Min_',num2str(k));
+            minint = featMat.(fn)(:,1:19);
+            minpre = featMat.(fn)(:,20:38);
+            testVect = abs([testVect; minint; minpre]);
+        end
+    end
+    % Ytest = repmat(Ytest,[j,1]);
+    % TestVec = [DeltTest(:,1) ; ThetTest(:,1) ; AlphTest(:,1) ;
+    %     BetaTest(:,1) ; GammTest(:,1) ; DeltTest(:,2) ; ThetTest(:,2) ; ...
+    %     AlphTest(:,2) ; BetaTest(:,2) ; GammTest(:,2)];
+    % TestLabel = predict(SVMModel, TestVec);
+    
+    TrainingTestLabel = predict(EEGSVMModel, testVect);
+elseif strcmp(runType,'test')
+    testVect = [];
+    for j = 1:nTrials
+        featMat = feature_matrix(ptNum, j, runType);
+        for k = 1:nwin
+            fn = strcat('Min_',num2str(k));
+            minDS = featMat.(fn)(:,1:19);
+            testVect = abs([testVect; minDS]);
+        end
+    end
+    TestLabel = predict(EEGSVMModel, testVect);
 end
 
-% figure(1)
-% plot(repmat(1:16,nwin*nTrials),meansTime(:,1),'+r')
-% hold on
-% plot(repmat(1:16,nwin*nTrials),meansTime(:,2),'vb')
-% pTitle = strcat('Mean Magnitude in Time for Pt', num2str(ptNum));
-% title(pTitle)
-% legend('InterIctal','PreIctal')
-% xlabel('Channel')
-% ylabel('Magnitude')
-% 
-% figure(2)
-% plot(repmat(1:16,nwin*nTrials),real(meansFFT(:,1)),'+r')
-% hold on
-% plot(repmat(1:16,nwin*nTrials),real(meansFFT(:,2)),'vb')
-% pTitle = strcat('Mean Magnitude in Frequency for Pt', num2str(ptNum));
-% title(pTitle)
-% legend('InterIctal','PreIctal')
-% xlabel('Channel')
-% ylabel('Magnitude')
-
-figure(3)
-plot(repmat(1:16,nwin*nTrials),stdTime(:,1),'+r')
-hold on
-plot(repmat(1:16,nwin*nTrials),stdTime(:,2),'vb')
-pTitle = strcat('Mean Standard Dev in Time for Pt', num2str(ptNum));
-title(pTitle)
-legend('InterIctal','PreIctal')
-xlabel('Channel')
-ylabel('Magnitude')
-
-figure(4)
-plot(repmat(1:16,nwin*nTrials),real(stdFFT(:,1)),'+r')
-hold on
-plot(repmat(1:16,nwin*nTrials),real(stdFFT(:,2)),'vb')
-pTitle = strcat('Mean Standard Dev in Frequency for Pt', num2str(ptNum));
-title(pTitle)
-legend('InterIctal','PreIctal')
-xlabel('Channel')
-ylabel('Magnitude')
-
-SVMModels = cell(2,1);
-classes = ['interIctal','preIctal']; 
+% TestLabel(:,2) = repmat([zeros(16,1); ones(16,1)],[30,1]);
+% compare = (TestLabel(:,1)==TestLabel(:,2));
+% mean(compare)
 
--- feature_matrix.m
+++ feature_matrix.m
@@ -1,4 +1,4 @@
-function featMat = feature_matrix(ptNum, j)
+function featMat = feature_matrix(ptNum, j, runType)
 %% Code Outline
 % Phase 1: Reading the Data
 %   1) Use eeg_read_svm.m to read in the pre-processed data
@@ -15,67 +15,145 @@ tic %used to time the program (how long it takes to run)
 nwin = 10; % number of 1-minute windows
 nch = 16; % number of channels
 
-[intIctmean, preIctmean, intIctmax, preIctmax, intIctstd, preIctstd, ...
-    fftintIctmean, fftpreIctmean, fftintIctmax, fftpreIctmax, ...
-    fftintIctstd, fftpreIctstd, intIctDelt, preIctDelt, intIctThet, ...
-    preIctThet, intIctAlph, preIctAlph, intIctBeta, preIctBeta, ...
-    intIctGamm, preIctGamm] = deal(zeros(nwin,nch));
-[intIctfreqMax, preIctfreqMax] = deal(zeros(nwin));
-
-
 %% Phase 1: Reading the Data
-[intIctDSwin, preIctDSwin, fftintIctDSwin, fftpreIctDSwin, fs] = eeg_read_svm(ptNum, j);
+if strcmp(runType,'train')
+    [intMean, preMean, intMax, preMax, intStd, preStd, ...
+        fftintMean, fftpreMean, fftintMedian, fftpreMedian, ...
+        fftintMax, fftpreMax, fftintStd, fftpreStd, ...
+        intDelt, preDelt, intThet, preThet, intAlph, ...
+        preAlph, intBeta, preBeta, intGamm, preGamm, ...
+        intActivity, preActivity, intMobility, preMobility, ...
+        intComplexity, preComplexity, intEntropy, preEntropy, ...
+        intCov, preCov, fftintCov, fftpreCov] = ...
+        deal(zeros(nwin,nch));
+    [intFreqMax, preFreqMax] = deal(zeros(nwin,1));
+    [intDSwin, preDSwin, fftintDSwin, fftpreDSwin, fs] = ...
+        eeg_read_svm(ptNum, j, runType);
+elseif strcmp(runType,'test')
+    [DSmean, fftDSmean, fftDSmedian, DSmax, fftDSmax, DSstd, fftDSstd, DSDelt, ...
+        DSThet, DSAlph, DSBeta, DSGamm, DSactivity, DSmobility, ...
+        DScomplexity, DSEntropy, DScov, fftDScov] = ...
+        deal(zeros( nwin, nch));
+    DSfreqMax = zeros(nwin,1);
+    [DSwin, fftDSwin, fs] = eeg_read_svm(ptNum, j, runType);
+end
 
 %% Phase 2: Mean, Max, and Std Dev; Covariance Matrix
 for k = 1:nwin
     fn = strcat('Min_',num2str(k));
     for l = 1:nch
-        intIctmean(k,l) = mean(intIctDSwin.(fn)(:,l));
-        preIctmean(k,l) = mean(preIctDSwin.(fn)(:,l));
-        intIctmax(k,l) = max(intIctDSwin.(fn)(:,l));
-        preIctmax(k,l) = max(preIctDSwin.(fn)(:,l));
-        intIctstd(k,l) = std(intIctDSwin.(fn)(:,l));
-        preIctstd(k,l) = std(preIctDSwin.(fn)(:,l));
-        fftintIctmean(k,l) = mean(fftintIctDSwin.(fn)(:,l));
-        fftpreIctmean(k,l) = mean(fftpreIctDSwin.(fn)(:,l));
-        [fftintIctmax(k,l), intIctfreqMax(k,l)] = max(fftintIctDSwin.(fn)(:,l));
-        [fftpreIctmax(k,l), preIctfreqMax(k,l)] = max(fftpreIctDSwin.(fn)(:,l));
-        fftintIctstd(k,l) = std(fftintIctDSwin.(fn)(:,l));
-        fftpreIctstd(k,l) = std(fftpreIctDSwin.(fn)(:,l));
-        intIctDelt(k,l) = bandpower(intIctDSwin.(fn)(:,l),fs,[0 4]);
-        intIctThet(k,l) = bandpower(intIctDSwin.(fn)(:,l),fs,[4 8]);
-        intIctAlph(k,l) = bandpower(intIctDSwin.(fn)(:,l),fs,[8 15]);
-        intIctBeta(k,l) = bandpower(intIctDSwin.(fn)(:,l),fs,[15 31]);
-        intIctGamm(k,l) = bandpower(intIctDSwin.(fn)(:,l),fs,[32 100]);
-        preIctDelt(k,l) = bandpower(preIctDSwin.(fn)(:,l),fs,[4 7]);
-        preIctThet(k,l) = bandpower(preIctDSwin.(fn)(:,l),fs,[4 8]);
-        preIctAlph(k,l) = bandpower(preIctDSwin.(fn)(:,l),fs,[8 15]);
-        preIctBeta(k,l) = bandpower(preIctDSwin.(fn)(:,l),fs,[15 31]);
-        preIctGamm(k,l) = bandpower(preIctDSwin.(fn)(:,l),fs,[32 100]);
+        if strcmp(runType,'train')
+            % Mean:
+            intMean(k,l) = mean(intDSwin.(fn)(:,l));
+            preMean(k,l) = mean(preDSwin.(fn)(:,l));
+            fftintMean(k,l) = mean(fftintDSwin.(fn)(:,l));
+            fftpreMean(k,l) = mean(fftpreDSwin.(fn)(:,l));
+            % Median:
+            fftintMedian(k,l) = median(fftintDSwin.(fn)(:,l));
+            fftpreMedian(k,l) = median(fftpreDSwin.(fn)(:,l));
+            % Max:
+            intMax(k,l) = max(intDSwin.(fn)(:,l));
+            preMax(k,l) = max(preDSwin.(fn)(:,l));
+            [fftintMax(k,l), intFreqMax(k,l)] = max(fftintDSwin.(fn)(:,l));
+            [fftpreMax(k,l), preFreqMax(k,l)] = max(fftpreDSwin.(fn)(:,l));
+            % Standard Deivation:
+            intStd(k,l) = std(intDSwin.(fn)(:,l));
+            preStd(k,l) = std(preDSwin.(fn)(:,l));
+            fftintStd(k,l) = std(fftintDSwin.(fn)(:,l));
+            fftpreStd(k,l) = std(fftpreDSwin.(fn)(:,l));
+            % Bandpower
+            intDelt(k,l) = bandpower(intDSwin.(fn)(:,l),fs,[0 4]);
+            intThet(k,l) = bandpower(intDSwin.(fn)(:,l),fs,[4 8]);
+            intAlph(k,l) = bandpower(intDSwin.(fn)(:,l),fs,[8 15]);
+            intBeta(k,l) = bandpower(intDSwin.(fn)(:,l),fs,[15 31]);
+            intGamm(k,l) = bandpower(intDSwin.(fn)(:,l),fs,[32 100]);
+            preDelt(k,l) = bandpower(preDSwin.(fn)(:,l),fs,[4 7]);
+            preThet(k,l) = bandpower(preDSwin.(fn)(:,l),fs,[4 8]);
+            preAlph(k,l) = bandpower(preDSwin.(fn)(:,l),fs,[8 15]);
+            preBeta(k,l) = bandpower(preDSwin.(fn)(:,l),fs,[15 31]);
+            preGamm(k,l) = bandpower(preDSwin.(fn)(:,l),fs,[32 100]);
+            % Hjorth parameters:
+            intdiff = diff(intDSwin.(fn)(:,l));
+            prediff = diff(preDSwin.(fn)(:,l));
+            intActivity(k,l) = var(intDSwin.(fn)(:,l));
+            preActivity(k,l) = var(preDSwin.(fn)(:,l));
+            intMobility(k,l) = std(intdiff)./std(intDSwin.(fn)(:,l));
+            preMobility(k,l) = std(prediff)./std(preDSwin.(fn)(:,l));
+            intComplexity(k,l) = std(diff(intdiff))./std(intdiff)./...
+                intMobility(k,l);
+            preComplexity(k,l) = std(diff(prediff))./std(prediff)./...
+                preMobility(k,l);
+            % Shannon Entropy:
+            intEntropy(k,l) = wentropy(intDSwin.(fn)(:,l),'shannon');
+            preEntropy(k,l) = wentropy(preDSwin.(fn)(:,l),'shannon');
+        elseif strcmp(runType,'test')
+            % Mean
+            DSmean(k,l) = mean(DSwin.(fn)(:,l));
+            fftDSmean(k,l) = mean(fftDSwin.(fn)(:,l));
+            % Median
+            fftDSmedian(k,l) = median(fftDSwin.(fn)(:,l));
+            % Max
+            DSmax(k,l) = max(DSwin.(fn)(:,l));
+            [fftDSmax(k,l), DSfreqMax(k,l)] = max(fftDSwin.(fn)(:,l));
+            % Standard Deviation
+            DSstd(k,l) = std(DSwin.(fn)(:,l));
+            fftDSstd(k,l) = std(fftDSwin.(fn)(:,l));
+            % Bandpower
+            DSDelt(k,l) = bandpower(DSwin.(fn)(:,l),fs,[0 4]);
+            DSThet(k,l) = bandpower(DSwin.(fn)(:,l),fs,[4 8]);
+            DSAlph(k,l) = bandpower(DSwin.(fn)(:,l),fs,[8 15]);
+            DSBeta(k,l) = bandpower(DSwin.(fn)(:,l),fs,[15 31]);
+            DSGamm(k,l) = bandpower(DSwin.(fn)(:,l),fs,[32 100]);
+            % Hjorth Parameters:
+            DSdiff = diff(DSwin.(fn)(:,l));
+            DSactivity(k,l) = var(DSwin.(fn)(:,l));
+            DSmobility(k,l) = std(DSdiff)./std(DSwin.(fn)(:,l));
+            DScomplexity(k,l) = std(diff(DSdiff))./std(DSdiff)./...
+                DSmobility(k,l);      
+            % Shannon Entropy:
+            DSEntropy(k,l) = wentropy(DSwin.(fn)(:,l),'shannon');
+        end
+    
     end
-    intIctmean(k, l+1) = mean(intIctmean(k,:));
-    preIctmean(k, l+1) = mean(preIctmean(k,:));
-    intIctmax(k, l+1) = mean(intIctmax(k,:));
-    preIctmax(k, l+1) = mean(preIctmax(k,:));
-    intIctstd(k, l+1) = mean(intIctstd(k,:));
-    preIctstd(k, l+1) = mean(preIctstd(k,:));
-    fftintIctmean(k, l+1) = mean(fftintIctmean(k,:));
-    fftpreIctmean(k, l+1) = mean(fftpreIctmean(k,:));
-    fftintIctmax(k, l+1) = mean(fftintIctmax(k,:));
-    fftpreIctmax(k, l+1) = mean(fftpreIctmax(k,:));
-    fftintIctstd(k, l+1) = mean(fftintIctstd(k,:));
-    fftpreIctstd(k, l+1) = mean(fftpreIctstd(k,:));
-    intIctcov = cov(intIctDSwin.(fn));
-    preIctcov = cov(preIctDSwin.(fn));
-    fftintIctcov = cov(fftintIctDSwin.(fn));
-    fftpreIctcov = cov(fftpreIctDSwin.(fn));
-    featMat.(fn) = [intIctmean(k,1:l)' preIctmean(k,1:l)' ...
-        fftintIctmean(k,1:l)' fftpreIctmean(k,1:l)' ...
-        intIctstd(k,1:l)' preIctstd(k,1:l)' fftintIctstd(k,1:l)' ...
-        fftpreIctstd(k,1:l)' intIctDelt(k,:)' preIctDelt(k,:)' ...
-        intIctThet(k,:)' preIctThet(k,:)' intIctAlph(k,:)' ...
-        preIctAlph(k,:)' intIctBeta(k,:)' preIctBeta(k,:)' ...
-        intIctGamm(k,:)' preIctGamm(k,:)'];
+%     intIctmean(k, l+1) = mean(intIctmean(k,:));
+%     preIctmean(k, l+1) = mean(preIctmean(k,:));
+%     intIctmax(k, l+1) = mean(intIctmax(k,:));
+%     preIctmax(k, l+1) = mean(preIctmax(k,:));
+%     intIctstd(k, l+1) = mean(intIctstd(k,:));
+%     preIctstd(k, l+1) = mean(preIctstd(k,:));
+%     fftintIctmean(k, l+1) = mean(fftintIctmean(k,:));
+%     fftpreIctmean(k, l+1) = mean(fftpreIctmean(k,:));
+%     fftintIctmax(k, l+1) = mean(fftintIctmax(k,:));
+%     fftpreIctmax(k, l+1) = mean(fftpreIctmax(k,:));
+%     fftintIctstd(k, l+1) = mean(fftintIctstd(k,:));
+%     fftpreIctstd(k, l+1) = mean(fftpreIctstd(k,:));
+    if strcmp(runType,'train')
+        intCov(k,:) = max(abs(cov(intDSwin.(fn))));
+        preCov(k,:) = max(abs(cov(preDSwin.(fn))));
+        fftintCov(k,:) = max(abs(cov(fftintDSwin.(fn))));
+        fftpreCov(k,:) = max(abs(cov(fftpreDSwin.(fn))));
+        featMat.(fn) = [intMean(k,:)' fftintMedian(k,:)' fftintMean(k,:)' ...
+            intMax(k,:)' fftintMax(k,:)' intFreqMax(k,:)' intStd(k,:)' ...
+            fftintStd(k,:)' intDelt(k,:)' intThet(k,:)' ...
+            intAlph(k,:)' intBeta(k,:)'  intGamm(k,:)' ...
+            intActivity(k,:)' intMobility(k,:)' intComplexity(k,:)' ...
+            intEntropy(k,:)' intCov(k,:)' fftintCov(k,:)' ...
+            preMean(k,:)' fftpreMedian(k,:)' fftpreMean(k,:)' ...
+            preMax(k,:)' fftpreMax(k,:)' preFreqMax(k,:)' ...
+            preStd(k,:)' fftpreStd(k,:)' preDelt(k,:)' preThet(k,:)' ...
+            preAlph(k,:)' preBeta(k,:)' preGamm(k,:)' ...
+            preActivity(k,:)' preMobility(k,:)' preComplexity(k,:)' ...
+            preEntropy(k,:)' preCov(k,:)' fftpreCov(k,:)'];
+    elseif strcmp(runType,'test')
+        DScov(k,:) = max(abs(cov(DSwin.(fn))));
+        fftDScov(k,:) = max(abs(cov(fftDSwin.(fn))));
+        featMat.(fn) = [DSmean(k,:)' fftDSmean(k,:)' fftDSmedian(k,:)' ...
+            DSmax(k,:)' fftDSmax(k,:)' DSfreqMax(k,:)' DSstd(k,:)' ...
+            fftDSstd(k,:)' DSDelt(k,:)' DSThet(k,:)' DSAlph(k,:)' ...
+            DSBeta(k,:)' DSGamm(k,:)' DSactivity(k,:)' DSmobility(k,:)' ...
+            DScomplexity(k,:)' DSEntropy(k,:)' DScov(k,:)' ... 
+            fftDScov(k,:)'];
+    end
 end
 
 TimeSpent = toc;
